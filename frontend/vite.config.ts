38dc083207fd577c888fab02937569fc2c6b7270
AynGhost099 <nandohervee@gmail.com>
2026-05-16 01:46:32 +0000

    Set base path for Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/static/',  // ← AJOUTE CECI
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
